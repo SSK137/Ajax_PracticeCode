80a477371cf4f5187c1da49547be5475f60b6173
SSK137 <saurabhkodam@gmail.com>
2022-06-21 09:04:52 +0530

    Added Functions to demonstrate Async nature of JS

--- Sample.js
+++ Sample.js
@@ -1,1 +1,13 @@
 console.log("Welcome To Ajax Practice Problems");
+function showTime(){
+    let date=new Date();
+    return date.getHours()+"Hrs "+date.getMinutes()+"Min "+date.getSeconds()+"Sec";
+}
+
+function showSessionExpire(){
+    console.log("Activity-B : Session Exired at "+showTime());
+}
+
+console.log("Activity-A : Triggering Activity-B at "+showTime());
+setTimeout(showSessionExpire,5000);
+console.log("Activity-A : Triggering Activity-B at "+showTime()+" Will execute after 5 sec.");
